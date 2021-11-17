78ab170f7bbc05dea018844e60b8362392683cc1
reivajpg <javierpg0515@gmail.com>
2021-11-17 07:23:04 -0500

    ircodes

---
+++ data/ircodes.js
@@ -0,0 +1,200 @@
+
+    var smcode = [] ;   // Codes for Samsung
+    var itcode = [] ;	// Codes for KPN-ITV
+    var lgcode = [] ;	// Codes for LG tv
+
+    smcode["power"]    = "C2CA807F" ;
+    smcode["tvscr"]    = "C2CA8877" ;
+    smcode["bd"]       = "" ;
+    smcode["tv"]       = "" ;
+    smcode["eject"]    = "C2CA0CF3" ;
+    smcode["sleep"]    = "C2CA847B" ;
+    smcode["one"]      = "CACA827D" ;
+    smcode["two"]      = "C2CA42BD" ;
+    smcode["three"]    = "C2CAC23D" ;
+    smcode["four"]     = "C2CA22DD" ;
+    smcode["five"]     = "C2CAA25D" ;
+    smcode["six"]      = "C2CA629D" ;
+    smcode["seven"]    = "C2CAE21D" ;
+    smcode["eight"]    = "C2CAFC03" ;
+    smcode["nine"]     = "C2CAEC13" ;
+    smcode["fullscrn"] = "C2CA0EF1" ;
+    smcode["zero"]     = "C2CAF40B" ;
+    smcode["repeat"]   = "C2CA6699" ;
+    smcode["skipback"] = "C2CAD827" ;
+    smcode["skip"]     = "C2CAC837" ;
+    smcode["rewind"]   = "C2CA2ED1" ;
+    smcode["forward"]  = "C2CAAE51" ;
+    smcode["stop"]     = "C2CAD02F" ;
+    smcode["play"]     = "C2CAC03F" ;
+    smcode["pause"]    = "C2CABE41" ;
+    smcode["volplus"]  = "C2CACC33" ;
+    smcode["mute"]     = "C2CA9C63" ;
+    smcode["prgplus"]  = "C2CAC43B" ;
+    smcode["volmin"]   = "C2CADC23" ;
+    smcode["vsound"]   = "C2CAE619" ;
+    smcode["prgmin"]   = "C2CAD42B" ;
+    smcode["dskmenu"]  = "C2CA5AA5" ;
+    smcode["menu"]     = "C2CA6C93" ;
+    smcode["ttlmnu"]   = "C2CACA35" ;
+    smcode["tools"]    = "C2CA28D7" ;
+    smcode["curup"]    = "C2CAB04F" ;
+    smcode["info"]     = "C2CA649B" ;
+    smcode["curleft"]  = "C2CAA45B" ;
+    smcode["enter"]    = "C2CAA05F" ;
+    smcode["curright"] = "C2CAB847" ;
+    smcode["return"]   = "C2CA38C7" ;
+    smcode["curdown"]  = "C2CAA857" ;
+    smcode["exit"]     = "C2CA7E81" ;
+    smcode["red"]      = "C2CADA25" ;
+    smcode["green"]    = "C2CA2AD5" ;
+    smcode["yellow"]   = "C2CAAA55" ;
+    smcode["blue"]     = "C2CAEE11" ;
+    smcode["smart"]    = "C2CADE21" ;
+    smcode["search"]   = "C2CA817E" ;
+    smcode["level"]    = "C2CAF00F" ;
+    smcode["threed"]   = "C2CA41BE" ;
+    smcode["sound"]    = "C2CAC13E" ;
+    smcode["free"]     = "" ;
+    smcode["dsp"]      = "C2CA5EA1" ;
+    smcode["vtuner"]   = "C2CA21DE" ;
+
+    itcode["tv"]       = "" ;
+    itcode["power"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMSMsmSm" ;
+    itcode["rewind"]   = "mSMSmsMSMSmsmsmsmsmsmsmsMSmsmsmsMSm" ;
+    itcode["play"]     = "mSMSmsMSMSmsmsmsmsmsmsmsmsMsmSMsmSm" ;
+    itcode["forward"]  = "mSMSmsMSMSmsmsmsmsmsmsmsMSmsMSmsM" ;
+    itcode["stop"]     = "mSMSmsMSMSmsmsmsmsmsmsmsMSmsmsMSM" ;
+    itcode["record"]   = "mSMSmsMSMSmsmsmsmsmsmsmsMSmsmsMsmSm" ;
+    itcode["red"]      = "mSMSmsMSMSmsmsmsmsmsmsmsMSmsmsMsmSm" ;
+    itcode["green"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsMsmsmsmSM" ;
+    itcode["yellow"]   = "mSMSmsMSMSmsmsmsmsmsmsmsmsMsmsmsmsmSm" ;
+    itcode["blue"]     = "mSMSmsMSMSmsmsmsmsmsmsmsMSmsmsmsmsM" ;
+    itcode["curup"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMsmSmsM" ;
+    itcode["curleft"]  = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMsmsmSM" ;
+    itcode["ok"]       = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMsmsmsmSm" ;
+    itcode["curright"] = "mSMSmsMSMSmsmsmsmsmsmsmsmsMSmsmsmsM" ;
+    itcode["curdown"]  = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMsmSMSm" ;
+    itcode["menu"]     = "mSMSmsMSMSmsmsmsmsmsmsmsMSMsmSMSm" ;
+    itcode["home"]     = "mSMSmsMSMSmsmsmsmsmsmsmsmsMSMSmsM" ;
+    itcode["guide"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsMSMsmSM" ;
+    itcode["radio"]    = "mSMSmsMSMSmsmsmsmsmsmsmsMSMSMsmSm" ;
+    itcode["return"]   = "mSMSmsMSMSmsmsmsmsmsmsmsmsMSmsMsmSm" ;
+    itcode["volplus"]  = "" ;
+    itcode["prgplus"]  = "mSMSmsMSMSmsmsmsmsmsmsmsmsMsmSmsMSm" ;
+    itcode["volmin"]   = "" ;
+    itcode["mute"]     = "" ;
+    itcode["prgmin"]   = "mSMSmsMSMSmsmsmsmsmsmsmsmsMsmsmSmsM" ;
+    itcode["one"]      = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsmsmsMSm" ;
+    itcode["two"]      = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsmsMSM" ;
+    itcode["three"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsmsMsmSm" ;
+    itcode["four"]     = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsMSmsM" ;
+    itcode["five"]     = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsMSMSm" ;
+    itcode["six"]      = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsMsmSM" ;
+    itcode["seven"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsmsMsmsmSm" ;
+    itcode["eight"]    = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMSmsmsM" ;
+    itcode["nine"]     = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMSmsMSm" ;
+    itcode["opt"]      = "" ;
+    itcode["zero"]     = "mSMSmsMSMSmsmsmsmsmsmsmsmsmsMSMSM" ;
+    itcode["book"]     = "mSMSmsMSMSmsmsmsmsmsmsmsMSMSmsMSm" ;
+
+    lgcode["power"]    = "20DF10EF" ;
+    lgcode["tvrad"]    = "20DF0FF0" ;
+    lgcode["saving"]   = "20DFA956" ;
+    lgcode["info"]     = "20DF55AA" ;
+    lgcode["input"]    = "20DFD02F" ;
+    lgcode["one"]      = "20DF8877" ;
+    lgcode["two"]      = "20DF48B7" ;
+    lgcode["three"]    = "20DFC837" ;
+    lgcode["four"]     = "20DF28D7" ;
+    lgcode["five"]     = "20DFA857" ;
+    lgcode["six"]      = "20DF6897" ;
+    lgcode["seven"]    = "20DFE817" ;
+    lgcode["eight"]    = "20DF1817" ;
+    lgcode["nine"]     = "20DF9867" ;
+    lgcode["list"]     = "20DFCA35" ;
+    lgcode["zero"]     = "20DF08F7" ;
+    lgcode["qview"]    = "20DF58A7" ;
+    lgcode["volplus"]  = "20DF40BF" ;
+    lgcode["fav"]      = "20DF7887" ;
+    lgcode["prgplus"]  = "20DF00FF" ;
+    lgcode["volmin"]   = "20DFC03F" ;
+    lgcode["mute"]     = "20DF906F" ;
+    lgcode["prgmin"]   = "20DF807F" ;
+    lgcode["menu"]     = "20DFC23D" ;
+    lgcode["qmenu"]    = "20DFA25D" ;
+    lgcode["curup"]    = "20DF02FD" ;
+    lgcode["curleft"]  = "20DFE01F" ;
+    lgcode["ok"]       = "20DF22DD" ;
+    lgcode["curright"] = "20DF609F" ;
+    lgcode["curdown"]  = "20DF827D" ;
+    lgcode["guide"]    = "20DFD52A" ;
+    lgcode["return"]   = "20DF14EB" ;
+    lgcode["red"]      = "20DF4EB1" ;
+    lgcode["green"]    = "20DF8E71" ;
+    lgcode["yellow"]   = "20DFC639" ;
+    lgcode["blue"]     = "20DF8679" ;
+    lgcode["tekst"]    = "20DF04FB" ;
+    lgcode["topt"]     = "20DF847B" ;
+    lgcode["subttl"]   = "20DF9C63" ;
+    lgcode["stop"]     = "20DF8D72" ;
+    lgcode["play"]     = "20DF0DF2" ;
+    lgcode["pause"]    = "20DF5DA2" ;
+    lgcode["rewind"]   = "20DFF10E" ;
+    lgcode["forward"]  = "20DF718E" ;
+    lgcode["link"]     = "20DF7E81" ;
+    lgcode["ratio"]    = "20DF9E61" ;
+    lgcode["avmode"]   = "20DF0CF3" ;
+
+
+    // Send a request to the ESP8266
+    function httpGet ( theReq )
+    {
+      var theUrl = "/" + theReq + "&version=" + Math.random() ;
+      var xhr = new XMLHttpRequest() ;
+      xhr.onreadystatechange = function() {
+        if ( xhr.readyState == XMLHttpRequest.DONE )
+        {
+          //resultstr.value = xhr.responseText ;
+        }
+      }
+      xhr.open ( "GET", theUrl, true ) ;
+      xhr.send ( null ) ;
+      document.getElementById ( 'tick' ).play() ;
+    } 
+
+
+    // Button (area) of Samsung remote control pressed
+    function smklik(a)
+    {
+      var hexcode = smcode[a] ;	// Hexcode to be send
+
+      if ( hexcode.length > 0 )
+      {
+        httpGet ( "?samsung&" + hexcode + "&" + a ) ;
+      }
+    }
+
+
+    // Button (area) of Samsung DVD remote control pressed
+    function itklik(a)
+    {
+      var itvcode = itcode[a] ;	// Hexcode to be send
+
+      if ( itvcode.length > 0 )
+      {
+        httpGet ( "?kpnitv&" + itvcode + "&" + a ) ;
+      }
+    }
+
+
+    // Button (area) of LG tv remote control pressed
+    function lgklik(a)
+    {
+      var hexcode = lgcode[a] ;	// Hexcode to be send
+
+      if ( hexcode.length > 0 )
+      {
+        httpGet ( "?lgtv&" + hexcode + "&" + a ) ;
+      }
+    }
